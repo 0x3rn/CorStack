939bb72be92b74112140c40274834892ba294e76
0x3rn <somto.ike.dev@gmail.com>
2026-05-08 11:45:26 +0100

    added a new file

--- server.js
+++ server.js
@@ -70,5 +70,8 @@ app.post('/api/checkout', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
+module.exports = app;
